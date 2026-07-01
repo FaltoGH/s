e7fb45af7686a3dcbf88b8cb7a82fc7c1d427895
FaltoGH <sb051380us@gmail.com>
2026-07-01 19:53:11 +0900

    Added Discord/.

---
+++ NaverNews/Discord/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection.Metadata;
+using System.Security.Policy;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml;
+
+namespace Discord
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine("error: argc must be 2");
+                return;
+            }
+
+            string requestUri = args[0];
+            string fileName = args[1];
+            string output;
+
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = fileName;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.Start();
+                process.WaitForExit();
+                int exitCode = process.ExitCode;
+                if (exitCode != 0)
+                {
+                    Console.Error.WriteLine($"error: process exit code is {exitCode}");
+                    return;
+                }
+                output = process.StandardOutput.ReadToEnd();
+            }
+
+            if (string.IsNullOrWhiteSpace(output))
+            {
+                Console.Error.WriteLine($"error: output is white space");
+                return;
+            }
+
+            HttpClient httpClient = new HttpClient();
+
+            if (output.Contains("```"))
+            {
+            }
+            else
+            {
+                output = "```\r\n" + output + "\r\n```";
+            }
+
+            Dictionary<string, string> values = new Dictionary<string, string>
+            {
+                ["content"] = output
+            };
+            FormUrlEncodedContent formUrlEncodedContent = new FormUrlEncodedContent(values);
+            HttpResponseMessage httpResponseMessage = httpClient.PostAsync(requestUri, formUrlEncodedContent).Result;
+            string responseString = httpResponseMessage.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(responseString);
+        }
+    }
+}
